4eb248d0db32108e0b8c0a8f6a2954bf217252cd
Bruno325 <152660029+Bruno325@users.noreply.github.com>
2024-02-02 01:20:02 -0500

    Update dec_to_display.v

--- dec_to_display.v
+++ dec_to_display.v
@@ -2,9 +2,7 @@
 module dec_to_display(input [3:0] num, output reg [7:0] display);
 
 	always @(*) begin
-	
 		case (num)
-			
 			4'd0: display = 8'b11000000;
 			4'd1: display = 8'b11111001;
 			4'd2: display = 8'b10100100;
@@ -15,10 +13,7 @@ module dec_to_display(input [3:0] num, output reg [7:0] display);
 			4'd7: display = 8'b11111000;
 			4'd8: display = 8'b10000000;
 			4'd9: display = 8'b10010000;
-			
-			
 		endcase
-	
 	end
-
+	
 endmodule
